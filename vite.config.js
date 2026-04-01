6c42d556dcebd05c9d6335da4091835c14e0500a
Heather Truong <heath9r@gmail.com>
2026-04-01 15:23:52 -0400

    initial commit for deploy

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
-import vuetify from 'vite-plugin-vuetify'
-
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [vue(), vuetify({autoImport: true})],
-  base: '/web-app-v4/',
-})
